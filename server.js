23e02d90d305bf371166084be1b130117a4b82be
lhomoludens <ysmnmlkc@gmail.com>
2025-07-14 23:32:45 -0400

    server.js has been updated
    Routes have been added to newly added pages

--- server.js
+++ server.js
@@ -5,12 +5,28 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => res.render('home', { title: 'Home' }));
 
 app.get('/about', (req, res) => res.render('about'));
-app.get('/testimonials', (req, res) => res.render('testimonials'));
-app.get('/contact', (req, res) => res.render('contact'));
-app.get('/career', (req, res) => res.render('career'));
+
+app.get('/blog', (req, res) => res.render('blog', { title: 'Blog' }));
+
+app.get('/menu', (req, res) => res.render('menu', { title: 'Menu' }));
+
 app.get('/order', (req, res) => res.render('order'));
+
+app.get('/locations', (req, res) => res.render('locations', { title: 'Locations' }));
+
+app.get('/testimonials', (req, res) => res.render('testimonials'));
+
+app.get('/events', (req, res) => res.render('events', { title: 'Events' }));
+
+app.get('/contact', (req, res) => res.render('contact'));
+
+app.get('/career', (req, res) => res.render('career'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
