207bd231bbf1f56f72ec4f1c7a494bf93e153b8d
lhomoludens <ysmnmlkc@gmail.com>
2025-08-14 15:00:27 -0400

    The dependencies definitions were made.
    "dotenv" and "mongoose" definitions were made in the server.js file.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const path = require('path');
+require('dotenv').config();
 
-app.use(express.static('public'));
+const app = express();
+
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, 'views'));
 
 const eventSchema = new mongoose.Schema({
